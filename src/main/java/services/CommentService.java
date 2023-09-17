a676d6b49a712c532f3dea0da61a5fdd4c1779f0
osparking <jbpark03@gmail.com>
2023-09-17 23:31:46 +0900

    interrupted method returns string

--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -11,9 +11,14 @@ import space.jbpark.utility.MyUtil;
 @Service
 public class CommentService {
 	private Logger logger = MyUtil.getLogger(CommentService.class.getName());
-	
+
 	@ToLog
-	public void publishComment(Comment comment) {
+	public String publishComment(Comment comment) {
 		logger.info("출판하는 코맨트 텍스트: " + comment.getText());
+		return "성공함";
+	}
+
+	public void editComment(Comment comment) {
+		logger.info("편집하는 코맨트: " + comment.getText());
 	}
 }
